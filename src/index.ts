2b3e2df8d2ee01e0f6e12e942e33aa9d42a69326
John Weland <john.weland@gmail.com>
2021-02-14 19:17:14 -0600

    basic unix socket server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-import * as net from 'net';
+import * as fs from 'fs';
+import { Server } from 'net';
+
+const HANDLE = '/tmp/some-file.sock';
+
+try {
+    fs.unlinkSync(HANDLE)
+} catch(err) {
+    if (err.code !== 'ENOENT') throw err
+}
 
 
-const PATH = '/var/run/generic/unix.sock';
-
-const server = net.createServer();
-server.listen(PATH, () => {
-    console.log(`unix domain socket server connected on  ${PATH}`);
+const server = new Server({});
+const chunks:Uint8Array[] = [];
+server.on('error', (err) => console.error(err));
+server.on('listening', () => console.info('listening'));
+server.on('connection', (client) => {
+    client.on('error', (err) => console.error(err));
+    client.on('data', chunk => chunks.push(chunk));
+    client.on('end', () => {
+        const payload = Buffer.concat(chunks)
+        console.info('end');
+    });
 });
 
-server.on('connection', (s)=> {
-    console.log(`connection made to unix domain socket server`);
-    s.write('hello-world');
-    s.end();
-});
+server.listen(HANDLE);
