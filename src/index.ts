6d1958a561b99e810e9c6d1bad7ae5b2b9e5351f
John Weland <john.weland@gmail.com>
2021-02-14 21:31:04 -0600

    code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import { Server } from 'net';
 
-const HANDLE = '/tmp/some-file.sock';
+const HANDLE = '/tmp/file.sock';
 
 try {
     fs.unlinkSync(HANDLE)
@@ -13,7 +13,7 @@ try {
 const server = new Server({});
 const chunks:Uint8Array[] = [];
 server.on('error', (err) => console.error(err));
-server.on('listening', () => console.info('listening'));
+server.on('listening', () => console.info(`listening on ${HANDLE}`));
 server.on('connection', (client) => {
     client.on('error', (err) => console.error(err));
     client.on('data', chunk => chunks.push(chunk));
